6e58e5bf86f414983b0f158eae27a8bf9d74d3f1
Saad Ahmed <ch.saad.ahmed@gmail.com>
2021-01-26 22:45:01 -0600

    testing again

--- main.ts
+++ main.ts
@@ -15,15 +15,15 @@ namespace tropic {
     /**
      * Pick a fruit
      */
-    //% blockId=tropic_pick block="pick a %fruit"
-    export function piiiik(fruit: TropicalFruit): boolean {
+    //% blockId=tropic_pick block="pick %fruit"
+    export function pick(fruit: TropicalFruit): boolean {
         return false;
     }
     /**
      * Peel the fruit if possible
      */
-    //% blockId=tropic_peel block="peel a %fruit"
-    export function piiil(fruit: TropicalFruit): boolean {
+    //% blockId=tropic_peel block="peel %fruit"
+    export function pee(fruit: TropicalFruit): boolean {
         return (fruit == TropicalFruit.Banana);
     }
 }
