578b40e1a798390d390946171e223e83dc0432d2
Saad Ahmed <ch.saad.ahmed@gmail.com>
2021-01-26 21:27:39 -0600

    testing

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
-})
+/**
+ * Types of tropical fruit
+ */
+enum TropicalFruit {
+    Banana = 0,
+    Pinapple = 1,
+    Coconut = 2
+}
+
+/**
+ * Pick some fruit and peel it.
+ */
+//% weight=70 icon="\uf185" color=#EC7505
+namespace tropic {
+    /**
+     * Pick a fruit
+     */
+    //% blockId=tropic_pick block="pick a %fruit"
+    export function pick(fruit: TropicalFruit): boolean {
+        return false;
+    }
+    /**
+     * Peel the fruit if possible
+     */
+    //% blockId=tropic_peel block="peel a %fruit"
+    export function peel(fruit: TropicalFruit): boolean {
+        return (fruit == TropicalFruit.Banana);
+    }
+}
